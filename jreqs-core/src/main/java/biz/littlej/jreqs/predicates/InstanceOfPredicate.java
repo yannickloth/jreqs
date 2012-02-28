596afeefe73b49d15fc5a2b7dc017ef38633ff9d
Yannick Loth <yannickloth@gmail.com>
2012-02-28 12:49:24 +0100

    Add some missing Javadoc and licence doc

--- jreqs-core/src/main/java/biz/littlej/jreqs/predicates/InstanceOfPredicate.java
+++ jreqs-core/src/main/java/biz/littlej/jreqs/predicates/InstanceOfPredicate.java
@@ -1,9 +1,28 @@
+/*
+ * Copyright (C) 2012 Yannick LOTH, LittleJ [www.littlej.biz]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package biz.littlej.jreqs.predicates;
 
 import biz.littlej.jreqs.Reqs;
 
 import java.io.Serializable;
 
+/**
+ * @author Yannick LOTH
+ * @since 0.1.0
+ */
 public class InstanceOfPredicate implements Predicate<Object>, Serializable {
     private static final long serialVersionUID = 0;
     private final Class<?> clazz;
